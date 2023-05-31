e13325f15a635fe73648f8cdd0d9bd5c28b34f18
arharuty <13937@coderacademy.edu.au>
2023-05-31 14:25:54 +1000

    Added Marshmellow

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
 
@@ -8,15 +9,63 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameg
 db = SQLAlchemy(app)
 
 class Card(db.Model):
+    __tablename__ = 'cards'
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.Text())
+    status = db.Column(db.String(30))
     date_created = db.Column(db.Date())
 
 @app.cli.command('create')
 def create_db():
+    db.drop_all()
     db.create_all()
     print('Tables created successfully')
+
+@app.cli.command('seed')
+def seed_db():
+    # Create instance of Card model in memory
+    cards = [
+    Card(
+        title = 'Start the project',
+        description = 'Stage 1 - Create an ERC',
+        status = 'Done',
+        date_created = date.today()
+    ),
+
+    Card(
+        title = 'ORM Queries',
+        description = 'Stage 2 - Implement several queries',
+        status = 'In Progress',
+        date_created = date.today()
+    ),
+
+    Card(
+        title = 'Marshmallow',
+        description = 'Stage 3 - Implement several queries',
+        status = 'In Progress',
+        date_created = date.today()
+    )    
+    ]
+
+    # Truncate the Card table
+    db.session.query(Card).delete()
+
+    # Add the card to the session (transaction)
+    db.session.add_all(cards)
+
+    # Commit the transaction to the database
+    db.session.commit()
+    print('Models seeded')
+
+@app.route('/cards')
+def all_cards():
+    # select * from cards;
+    ## in sqlalchemy language:
+    stmt = db.select(Card).order_by(Card.status.desc())
+    cards = db.session.scalars(stmt).all()
+    return cards
 
 @app.route('/')
 def index():
