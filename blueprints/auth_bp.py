1191be672819b6903aec00832d4eb7e8354c6d03
arharuty <13937@coderacademy.edu.au>
2023-06-08 12:00:13 +1000

    refactor out auth bp

---
+++ blueprints/auth_bp.py
@@ -0,0 +1,39 @@
+from flask import request, Blueprint
+from datetime import timedelta
+from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import create_access_token
+from models.user import User, UserSchema
+from init import db, bcrypt
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/register', methods=['POST'])
+def register():
+    try:
+        user_info = UserSchema().load(request.json)
+        user = User(
+            email=user_info['email'],
+            password=bcrypt.generate_password_hash(user_info['password']).decode('utf8'),
+            name = user_info['name']
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        return UserSchema(exclude=['password']).dump(user), 201
+    
+    except IntegrityError:
+        return {'error': 'Email address already in use'}, 409
+
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    try:
+        stmt = db.select(User).filter_by(email=request.json['email'])
+        user = db.session.scalar(stmt)
+        if user and bcrypt.check_password_hash(user.password, request.json['password']):
+            token = create_access_token(identity=user.email, expires_delta=timedelta(days=1))
+            return {'token': token, 'user': UserSchema(exclude=['password']).dump(user)}
+        else: 
+            return {'error': 'Invalid email address or password'}, 401
+    except KeyError:
+        return {'error': 'Email & password required'}, 400
