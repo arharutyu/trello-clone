160f320b1b791e3bc8909bbcfcd8c8f43347200f
arharuty <13937@coderacademy.edu.au>
2023-06-12 05:54:25 +1000

    update from missed class

--- blueprints/cli_bp.py
+++ blueprints/cli_bp.py
@@ -1,63 +1,69 @@
 from flask import Blueprint
-from models.user import User
 from datetime import date
+from models.user import User
 from models.card import Card
 from init import db, bcrypt
 
 cli_bp = Blueprint('db', __name__)
 
-@cli_bp.cli.command('create')
+@cli_bp.cli.command("create")
 def create_db():
     db.drop_all()
     db.create_all()
-    print('Tables created successfully')
+    print("Tables created successfully")
 
-@cli_bp.cli.command('seed')
+
+@cli_bp.cli.command("seed")
 def seed_db():
     users = [
         User(
-            email = 'admin@spam.com',
-            password = bcrypt.generate_password_hash('spineynorman').decode('utf-8'),
-            is_admin = True
+            email='admin@spam.com',
+            password=bcrypt.generate_password_hash('spinynorman').decode('utf-8'),
+            is_admin=True
         ),
         User(
-            name = 'John Cleese',
-            email = 'cleese@spam.com',
-            password = bcrypt.generate_password_hash('tisbutascratch').decode('utf-8')
+            name='John Cleese',
+            email='cleese@spam.com',
+            password=bcrypt.generate_password_hash('tisbutascratch').decode('utf-8')
         )
     ]
-    # Create instance of Card model in memory
+
+    db.session.query(User).delete()
+    db.session.add_all(users)
+    db.session.commit()
+
+    # Create an instance of the Card model in memory
     cards = [
-    Card(
-        title = 'Start the project',
-        description = 'Stage 1 - Create an ERC',
-        status = 'Done',
-        date_created = date.today()
-    ),
-
-    Card(
-        title = 'ORM Queries',
-        description = 'Stage 2 - Implement several queries',
-        status = 'In Progress',
-        date_created = date.today()
-    ),
-
-    Card(
-        title = 'Marshmallow',
-        description = 'Stage 3 - Implement several queries',
-        status = 'In Progress',
-        date_created = date.today()
-    )    
+        Card(
+            title="Start the project",
+            description="Stage 1 - Create an ERD",
+            status="Done",
+            date_created=date.today(),
+            user_id=users[0].id
+        ),
+        Card(
+            title="ORM Queries",
+            description="Stage 2 - Implement several queries",
+            status="In Progress",
+            date_created=date.today(),
+            user_id=users[0].id
+        ),
+        Card(
+            title="Marshmallow",
+            description="Stage 3 - Implement jsonify of models",
+            status="In Progress",
+            date_created=date.today(),
+            user_id=users[1].id
+        ),
     ]
 
     # Truncate the Card table
     db.session.query(Card).delete()
-    db.session.query(User).delete()
 
     # Add the card to the session (transaction)
     db.session.add_all(cards)
-    db.session.add_all(users)
 
     # Commit the transaction to the database
     db.session.commit()
-    print('Models seeded')
+    print("Models seeded")
+
